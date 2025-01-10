4ec929ae7bbb0d6c3b437391dbd620f8bf28fc9c
Erick Monteiro <85596186+BiraBalaZ@users.noreply.github.com>
2025-01-09 22:37:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 playerLife = 3
 iaLife = 3
 
-while iaLife > 0 or playerLife > 0:
+while (iaLife > 0 or playerLife > 0) and len(ammo) > 0:
     system('cls')
     turn = 'Player'
     choice = None
@@ -38,74 +38,73 @@ while iaLife > 0 or playerLife > 0:
     print(f'{realCount} cartuchos verdadeiros e {festimCount} cartuchos falsos.')
     sleep(2)
 
-    while (iaLife > 0 or playerLife > 0) and len(ammo) > 0:
-        printLife(playerLife, iaLife)
+    # while (iaLife > 0 or playerLife > 0) and len(ammo) > 0:
+    printLife(playerLife, iaLife)
 
-        if turn == 'Player':
-            choice = int(input('[0] Atirar em você\n[1] Atirar NELE\n>>>'))
-            system('cls')
-        elif turn == 'IA':
-            festimCount = ammo.count('Festim')
-            realCount = ammo.count('Real')
-            
-            # Fazendo ele "contar" quantas balas foram
-            if festimCount > 0 and realCount == 0:
-                choice = 1
-            elif festimCount == 0 and realCount > 0:
-                choice = 0
-            else:
-                choice = int(random.randrange(0, 2))
-            system('cls')
-
-        # Atirando no Jogador
-        if choice == 0:
-            if turn == 'Player':
-                print('Você aponta a arma para você mesmo')
-            else:
-                print('Ele aponta a arma para você')
-            sleep(1)
-
-            if ammo[0] == "Festim":
-                print('Click')
-                turn = 'Player'
-            else:
-                print('BANG')
-                playerLife -= 1
-                turn = 'IA'
-        # Atirando Nele
-        elif choice == 1:
-            if turn == 'Player':
-                print('Você aponta a arma para Ele')
-            else :
-                print('Ele aponta a arma para si mesmo')
-            sleep(1)
-
-            if ammo[0] == "Festim":
-                print('Click')
-                turn = 'IA'
-            else:
-                print('BANG')
-                iaLife -= 1
-                turn = 'Player'
+    if turn == 'Player':
+        choice = int(input('[0] Atirar em você\n[1] Atirar NELE\n>>>'))
+        system('cls')
+    elif turn == 'IA':
+        festimCount = ammo.count('Festim')
+        realCount = ammo.count('Real')
         
-        sleep(2)
-        # removing the first bullet
-        if len(ammo) > 0:
-            del ammo[0]
+        # Fazendo ele "contar" quantas balas foram
+        if festimCount > 0 and realCount == 0:
+            choice = 1
+        elif festimCount == 0 and realCount > 0:
+            choice = 0
+        else:
+            choice = int(random.randrange(0, 2))
+        system('cls')
 
-        # If the life of one of them is zero, the game ends
-        if iaLife == 0:
-            print('Você venceu\nFim de Jogo')
-            break
-        elif playerLife == 0:
-            print('Você perdeu...\nFim de Jogo')
-            break
+    # Atirando no Jogador
+    if choice == 0:
+        if turn == 'Player':
+            print('Você aponta a arma para você mesmo')
+        else:
+            print('Ele aponta a arma para você')
+        sleep(1)
 
-        # If the shotgun ammo is zero, the game restart with other Ammo Set
-        # print('Fim da rodada\nPróxima rodada!')
-        # continue
+        if ammo[0] == "Festim":
+            print('Click')
+            turn = 'Player'
+        else:
+            print('BANG')
+            playerLife -= 1
+            turn = 'IA'
+    # Atirando Nele
+    elif choice == 1:
+        if turn == 'Player':
+            print('Você aponta a arma para Ele')
+        else :
+            print('Ele aponta a arma para si mesmo')
+        sleep(1)
+
+        if ammo[0] == "Festim":
+            print('Click')
+            turn = 'IA'
+        else:
+            print('BANG')
+            iaLife -= 1
+            turn = 'Player'
+    
+    sleep(2)
+    # removing the first bullet
+    if len(ammo) > 0:
+        del ammo[0]
+
+    # If the life of one of them is zero, the game ends
+    if iaLife == 0:
+        print('Você venceu\nFim de Jogo')
+        break
+    elif playerLife == 0:
+        print('Você perdeu...\nFim de Jogo')
+        break
+
+    # If the shotgun ammo is zero, the game restart with other Ammo Set
+    # print('Fim da rodada\nPróxima rodada!')
+    # continue
 
     sleep(3)
     system('cls')
     print('Sem balas.\nPróxima rodada.')
-    
